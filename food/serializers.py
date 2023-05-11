df1a38c00c5af9a039d86c96e6dcaf666396f9ea
3759357 <94110438+3759357@users.noreply.github.com>
2023-05-11 15:48:56 +0900

    Delete food directory

--- food/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import food
-
-class foodSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = food
-        fields = ('title', 'text')
